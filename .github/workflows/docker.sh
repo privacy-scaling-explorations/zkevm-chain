54f53af998e1fcd7e95cb041a7fef758d81327c1
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-02-25 12:55:48 +0000

    add github workflow for building docker images

---
+++ .github/workflows/docker.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -ex
+
+tag=$(git tag --points-at HEAD)
+
+if [ -z "$tag" ]; then
+  tag='latest'
+fi
+
+base=$(pwd)
+
+docker buildx create --name mybuilder --use || echo 'skip'
+docker buildx inspect --bootstrap
+
+for file in docker/*/Dockerfile; do
+  cd $(dirname $file)
+  ext=${PWD##*/}
+  image="ghcr.io/$GITHUB_ACTION_REPOSITORY/$ext"
+  echo $image:$tag
+  docker buildx build --platform linux/amd64,linux/arm64 -t $image:$tag --push .
+  docker buildx imagetools inspect $image:$tag
+  cd $base
+done
