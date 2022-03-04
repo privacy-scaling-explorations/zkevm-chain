428e06978cf000d03404526574a286279e665ce5
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-03-04 14:44:34 +0000

    workflows: fix: --push missing

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -17,6 +17,6 @@ for file in docker/*/Dockerfile; do
   image="ghcr.io/$GITHUB_REPOSITORY/$ext"
   echo $image:$tag
   # zkevm-circuits/libc has linking problems with linux/arm64
-  docker buildx build --platform linux/amd64 -t $image:$tag -f $file .
+  docker buildx build --push --platform linux/amd64 -t $image:$tag -f $file .
   docker buildx imagetools inspect $image:$tag
 done
