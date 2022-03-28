5451a802103d98ca9557d674f4d745b1e6e83957
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-03-28 13:29:30 +0000

    docker/workflows: enable arm64 build

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -16,7 +16,6 @@ for file in docker/*/Dockerfile; do
   ext=${path##*/}
   image="ghcr.io/$GITHUB_REPOSITORY/$ext"
   echo $image:$tag
-  # zkevm-circuits/libc has linking problems with linux/arm64
-  docker buildx build --push --platform linux/amd64 -t $image:$tag -f $file .
+  docker buildx build --push --platform linux/amd64,linux/arm64 -t $image:$tag -f $file .
   docker buildx imagetools inspect $image:$tag
 done
