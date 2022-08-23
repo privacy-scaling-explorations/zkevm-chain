864ba4919e2e486d25ea6e1bbe00e4afc516cb44
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-08-23 17:22:03 +0000

    docker: add custom developer image with solc and support templating contracts

--- scripts/compile_contracts.sh
+++ scripts/compile_contracts.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-docker run --rm -w /app -v $(pwd):/app \
-  ethereum/solc:0.8.13 \
+ROOT="$(dirname $0)/.."
+
+for file in $(find "$ROOT"/contracts/templates/ -iname '*.sol'); do
+  name=$(basename $file)
+  generated="$ROOT"/contracts/generated/$name
+  cp "$file" "$generated"
+  for inc in $(cat "$file" | grep "//@INCLUDE:"); do
+    template=$(echo "$inc" | awk -F":" '{print $NF}')
+    sed -i -e "\#$inc#r $ROOT/contracts/templates/$template" "$generated"
+  done
+done
+
+SOLC=$(which solc || printf '%s' "docker run --rm -w /app -v $(pwd):/app ethereum/solc:0.8.16")
+$SOLC \
   --overwrite \
   --metadata-hash none \
   --asm-json \
@@ -10,6 +22,7 @@ docker run --rm -w /app -v $(pwd):/app \
   --bin-runtime \
   --abi \
   --optimize \
+  --optimize-runs 4294967295 \
   --userdoc \
-  -o build/contracts/ \
-  $(find contracts/ -iname '*.sol')
+  -o "$ROOT"/build/contracts/ \
+  $(find "$ROOT"/contracts/ -iname '*.sol' | grep -v templates/)
