c4eaae261ae34f26128496ef0f9801c34da92c9e
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-08-25 17:26:48 +0000

    workflows: add docker cache

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -15,5 +15,11 @@ path=$(dirname $DOCKERFILE)
 ext=${path##*/}
 image="ghcr.io/$GITHUB_REPOSITORY/$ext"
 
-docker buildx build --push --platform $PLATFORM -t $image:$tag -f $DOCKERFILE .
+docker buildx build \
+  --cache-from "type=local,src=$RUNNER_TEMP/docker-cache" \
+  --cache-to "type=local,dest=$RUNNER_TEMP/docker-cache-new" \
+  --push \
+  --platform $PLATFORM \
+  -t $image:$tag \
+  -f $DOCKERFILE .
 docker buildx imagetools inspect $image:$tag
