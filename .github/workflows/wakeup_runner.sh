9195a98ca0ef282c3e8886311235de0ac12d841c
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-10-06 11:15:25 +0000

    bashism strikes

--- .github/workflows/wakeup_runner.sh
+++ .github/workflows/wakeup_runner.sh
@@ -11,7 +11,7 @@ set -xe
 while true; do
   status=$(aws ec2 start-instances --instance-ids "$AWS_INSTANCE_ID" | jq -r .StartingInstances[0].CurrentState.Name)
   echo "$status"
-  if [ "$status" == "running" ]; then
+  if [ "$status" = "running" ]; then
     break
   fi
   sleep 3
