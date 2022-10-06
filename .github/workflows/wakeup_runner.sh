e9c15d0059c883eca59789e163ef42bb9e90dfe4
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-10-06 09:15:33 +0000

    workflows: heavy tests

---
+++ .github/workflows/wakeup_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -x
+
+# Requires the following env variables:
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+# AWS_DEFAULT_REGION
+# AWS_INSTANCE_ID
+
+while true; do
+  status=$(aws ec2 start-instances --instance-ids "$AWS_INSTANCE_ID" | jq -r .StartingInstances[0].CurrentState.Name)
+  echo "$status"
+  if [ "$status" == "running" ]; then
+    break
+  fi
+  sleep 3
+done
