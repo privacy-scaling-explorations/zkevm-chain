0d564c2531c1fc890038b053ac247ce78a911406
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-10-14 11:57:51 +0000

    upgrade clap to fix default env parsing errors

--- scripts/test_prover.sh
+++ scripts/test_prover.sh
@@ -8,4 +8,4 @@ pkill -9 prover_rpcd || true
 
 cargo build --release --bin prover_rpcd
 cargo run --release --bin prover_rpcd > PROVER_LOG.txt 2>&1 &
-COORDINATOR_DUMMY_PROVER=0 cargo test -- $@
+COORDINATOR_DUMMY_PROVER=false cargo test -- $@
