665b86bb5150f22ef7e37a531534d186b145e42b
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-10-26 10:15:34 +0200

    contracts: dry ZkEvmL2MessageDeliverer's block header procedure (#76)

---
+++ contracts/templates/CommonBlockOperations.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity <0.9.0;
+
+contract CommonBlockOperations {
+  function _readBlockHeader (
+    bytes calldata blockHeader
+  ) internal pure returns (bytes32 blockHash, bytes32 stateRoot, uint256 _timestamp) {
+    assembly {
+      //@INCLUDE:utils.yul
+      //@INCLUDE:rlp.yul
+
+      // expecting 16 individual items from the block header
+      let calldataPtr, memStart, nItems, hash := decodeFlat(blockHeader.offset)
+
+      // boundary check
+      if iszero( eq(calldataPtr, add(blockHeader.offset, blockHeader.length)) ) {
+        revertWith('BOUNDS')
+      }
+      if iszero( eq(nItems, 16) ) {
+        revertWith('ITEMS')
+      }
+
+      blockHash := hash
+
+      // at position 3 should be the stateRoot
+      stateRoot := loadValue(memStart, 3)
+
+      // at position 11 should be the timestamp
+      _timestamp := loadValue(memStart, 11)
+    }
+  }
+}
