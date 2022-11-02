59c979e8e43f0f580a4ff625d0a90396574fdf7c
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-11-02 13:37:43 +0000

    move patch genesis to workflow steps

--- scripts/ci_autogen_commit.sh
+++ scripts/ci_autogen_commit.sh
@@ -6,9 +6,7 @@ if [ -z "$CI" ]; then
   exit 1
 fi
 
-docker run --rm -v $(pwd):/host -w /host node:lts-alpine scripts/patch_genesis.mjs
-
 git config --global user.email 'bot@github.action'
 git config --global user.name 'github action'
-git commit -am 'updates from autogen workflow'
+git commit -am 'updates from autogen workflow' || exit 0
 git push
