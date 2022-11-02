56aa105ef494ce96c1835a70df0ad58a39caaf8a
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-11-02 10:35:51 +0000

    use docker for nodejs

--- scripts/ci_autogen_commit.sh
+++ scripts/ci_autogen_commit.sh
@@ -6,7 +6,8 @@ if [ -z "$CI" ]; then
   exit 1
 fi
 
-./scripts/patch_genesis.mjs
+docker run --rm -v $(pwd):/host -w /host node:lts-alpine scripts/patch_genesis.mjs
+
 git config --global user.email 'bot@github.action'
 git config --global user.name 'github action'
 git commit -am 'updates from autogen workflow'
