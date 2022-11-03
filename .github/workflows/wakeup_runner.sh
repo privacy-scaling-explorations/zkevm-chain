78a4fbca5f8d961db8efdc8bef633fdd61b14392
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-11-03 10:25:48 +0100

    wakeup_runner: exit if AWS_DEFAULT_REGION doesn't exist (#83)

--- .github/workflows/wakeup_runner.sh
+++ .github/workflows/wakeup_runner.sh
@@ -8,6 +8,10 @@ set -xe
 # AWS_DEFAULT_REGION
 # AWS_INSTANCE_ID
 
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  exit 1
+fi
+
 while true; do
   status=$(aws ec2 start-instances --instance-ids "$AWS_INSTANCE_ID" | jq -r .StartingInstances[0].CurrentState.Name)
   if [ "$status" = "running" ]; then
