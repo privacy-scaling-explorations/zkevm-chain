56211a4d780540c56b5e6003d4d0f4bc5b373451
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2023-01-20 09:19:40 +0000

    scripts: optional usage of perf util

--- scripts/test_prover.sh
+++ scripts/test_prover.sh
@@ -7,5 +7,11 @@ trap 'pkill --parent $$' TERM EXIT INT
 pkill -9 prover_rpcd || true
 
 cargo build --release --bin prover_rpcd
-perf record -e task-clock -F 1000 -g -- cargo run --release --bin prover_rpcd > PROVER_LOG.txt 2>&1 &
+
+if [ "${DEV_PERF}" ]; then
+  perf record -e task-clock -F 1000 -g -- cargo run --release --bin prover_rpcd > PROVER_LOG.txt 2>&1 &
+else
+  cargo run --release --bin prover_rpcd > PROVER_LOG.txt 2>&1 &
+fi
+
 COORDINATOR_DUMMY_PROVER=false cargo test -- $@
