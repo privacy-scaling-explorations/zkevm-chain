6924bd50eb2d65b60f3bfa79d1263384680a1e46
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2023-02-02 11:31:39 +0100

    CI improvements (#113)
    - use docker registry as cache
    - upgrade coverage tool
    - improve docker cache behaviour in integration tests

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -11,15 +11,18 @@ fi
 docker buildx create --name mybuilder --use || echo 'skip'
 docker buildx inspect --bootstrap
 
-path=$(dirname $DOCKERFILE)
+dockerfile="docker/${TARGET}/Dockerfile"
+path=$(dirname "${dockerfile}")
 ext=${path##*/}
 image="ghcr.io/$GITHUB_REPOSITORY/$ext"
 
 docker buildx build \
-  --cache-from "type=local,src=$RUNNER_TEMP/docker-cache" \
-  --cache-to "type=local,dest=$RUNNER_TEMP/docker-cache-new" \
+  --progress plain \
+  --cache-from "type=registry,ref=${image}-ci-cache:latest" \
+  --cache-to "type=registry,ref=${image}-ci-cache:latest,mode=max" \
+  --compress \
   --push \
-  --platform $PLATFORM \
-  -t $image:$tag \
-  -f $DOCKERFILE .
-docker buildx imagetools inspect $image:$tag
+  --platform "$PLATFORM" \
+  -t "$image:$tag" \
+  -f "${dockerfile}" .
+docker buildx imagetools inspect "$image:$tag"
