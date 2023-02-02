58939a3db9ec772a88038a841b2fbe5775b031ec
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2023-02-02 09:34:00 +0000

    test

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -10,8 +10,6 @@ fi
 
 docker buildx create --name mybuilder --use || echo 'skip'
 docker buildx inspect --bootstrap
-docker compose build dev
-exit
 
 dockerfile="docker/${TARGET}/Dockerfile"
 path=$(dirname "${dockerfile}")
@@ -21,17 +19,11 @@ image="ghcr.io/$GITHUB_REPOSITORY/$ext"
 docker buildx build \
   --cache-from "type=registry,ref=${image}-ci-cache:latest" \
   --cache-to "type=registry,ref=${image}-ci-cache:latest,mode=max" \
+  --compress \
   --push \
   --platform "$PLATFORM" \
   -t "$image:$tag" \
   -f "${dockerfile}" .
 docker buildx imagetools inspect "$image:$tag"
 
-docker buildx build \
-  --cache-from "type=registry,ref=${image}-ci-cache:latest" \
-  --cache-from "type=registry,ref=${image}-ci-cache-secondary:latest" \
-  --cache-to "type=registry,ref=${image}-ci-cache-secondary:latest,mode=max" \
-  --push \
-  -t "$image-ci:$tag" \
-  -f "${dockerfile}" .
-
+docker compose build dev
